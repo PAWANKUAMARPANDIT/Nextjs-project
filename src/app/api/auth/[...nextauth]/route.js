4d8bfd10d4f5bbbf3852196aa5867c28a1e304ec
Pawan Kr Pandit <pawanpandit9834@gmail.com>
2024-12-07 18:21:37 +0530

    error fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import { authenticateUser } from '../Login';
+import { authenticateUser } from '../Login/route';
 
 const handler = NextAuth({
   providers: [
